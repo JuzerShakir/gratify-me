6981b8d453847e65d000a4fa4330ec9d2a1a3a5b
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-11 16:10:57 +0530

    updated logic

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -8,9 +8,12 @@ class Users::CallbacksController < Devise::OmniauthCallbacksController
 
     def github
         @user = User.from_omniauth(request.env["omniauth.auth"])
-        unless sign_in @user
+        if User.find_by(email: @user.email)
             redirect_to :root, alert: 'The email your trying to login with is already in use. You may have signed earlier with a different provider'
+        else
+            sign_in @user
+            redirect_to :root
         end
-        redirect_to :root
+
     end
 end
