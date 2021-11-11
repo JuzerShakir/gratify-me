89b5a4a95018567bd78e852122dce1ccbc042675
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-11 22:43:01 +0530

    Hotwire implement on notes model of a user

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :user
+
+  include ActionView::RecordIdentifier
+
+  after_create_commit { broadcast_prepend_to [user, :notes], target: "#{dom_id(user)}_notes" }
+
 end
