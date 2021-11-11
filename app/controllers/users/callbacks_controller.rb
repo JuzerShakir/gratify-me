b53558e866b91e189f9a44f0dc89f030db631f09
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-11 16:24:17 +0530

    updated model level logic to save only if email is unique

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -8,10 +8,7 @@ class Users::CallbacksController < Devise::OmniauthCallbacksController
 
     def github
         @user = User.from_omniauth(request.env["omniauth.auth"])
-        if User.find_by(email: @user.email)
-            alert  'The email your trying to login with is already in use. You may have signed earlier with a different provider'
-            redirect_to 'youtube.com'
-        end
-
+        sign_in @user
+        redirect_to :root
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
           :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github google_oauth2]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do | user |
+    where(email: auth.info.email).first_or_create do | user |
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
