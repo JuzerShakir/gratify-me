570b1d793f106d91aeed92f2ffc01862cf2cae66
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-12 15:24:31 +0530

    Fix callback issue, give separate methods to separate logic

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -2,9 +2,13 @@ class Users::CallbacksController < Devise::OmniauthCallbacksController
     before_action :load_user
 
     def google_oauth2
+        existing_user if  @user.provider != __method__.match(/[a-z]+/).to_s.capitalize
+        new_user
     end
 
     def github
+        existing_user if  @user.provider != __method__.to_s.capitalize
+        new_user
     end
 
     private
@@ -12,11 +16,13 @@ class Users::CallbacksController < Devise::OmniauthCallbacksController
     def load_user
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in @user
+    end
 
-        if @user.provider != __method__.match(/[a-z]+/).to_s.capitalize
-            redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
-        else
-            redirect_to :root, notice: 'Signed in successfully!'
-        end
+    def existing_user
+        redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
+    end
+
+    def new_user
+        redirect_to :root, notice: 'Signed in successfully!'
     end
 end
