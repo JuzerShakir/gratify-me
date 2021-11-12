b0b7facca089b4821201ad471b2065376db2db26
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-12 15:45:40 +0530

    updated method names and logic

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -1,13 +1,20 @@
 class Users::CallbacksController < Devise::OmniauthCallbacksController
     before_action :load_user
-    after_action :show_user
 
     def google_oauth2
-        @method_name = __method__.match(/[a-z]+/).to_s.capitalize
+        if @user.provider == __method__.match(/[a-z]+/).to_s.capitalize
+            correct_provider
+        else
+            provider_already_exists
+        end
     end
 
     def github
-        @method_name = __method__.to_s.capitalize
+        if @user.provider == __method__.to_s.capitalize
+            correct_provider
+        else
+            provider_already_exists
+        end
     end
 
 
@@ -18,19 +25,11 @@ class Users::CallbacksController < Devise::OmniauthCallbacksController
         sign_in @user
     end
 
-    def existing_user
+    def provider_already_exists
         redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
     end
 
-    def new_user
+    def correct_provider
         redirect_to :root, notice: 'Signed in successfully!'
-    end
-
-    def show_user
-        if  @user.provider != @method_name
-            existing_user
-        else
-            new_user
-        end
     end
 end
