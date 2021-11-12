7cd94b88e45021011c006d783bc20dbf5d659613
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-12 14:56:31 +0530

    Callback provided for loading user and users who signs in via google where they have already signed in with other providers will receive a flash message

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -1,17 +1,26 @@
 class Users::CallbacksController < Devise::OmniauthCallbacksController
+    before_action :load_user
+
     def google_oauth2
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in @user
-        redirect_to :root, notice: 'Signed in successfully!'
+        if @user.provider != 'Google'
+            redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
+        else
+            redirect_to :root, notice: 'Signed in successfully!'
+        end
     end
 
     def github
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in @user
         if @user.provider != 'Github'
             redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
         else
             redirect_to :root, notice: 'Signed in successfully!'
         end
+    end
+
+    private
+
+    def load_user
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in @user
     end
 end
