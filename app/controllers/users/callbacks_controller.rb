b2e34611b93475e0b4c7a787c58900f79db2a0cb
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-12 15:10:32 +0530

    Full callback provided for Omniauth login, Follow DRY concept

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -2,19 +2,9 @@ class Users::CallbacksController < Devise::OmniauthCallbacksController
     before_action :load_user
 
     def google_oauth2
-        if @user.provider != 'Google'
-            redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
-        else
-            redirect_to :root, notice: 'Signed in successfully!'
-        end
     end
 
     def github
-        if @user.provider != 'Github'
-            redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
-        else
-            redirect_to :root, notice: 'Signed in successfully!'
-        end
     end
 
     private
@@ -22,5 +12,11 @@ class Users::CallbacksController < Devise::OmniauthCallbacksController
     def load_user
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in @user
+
+        if @user.provider != __method__.match(/[a-z]+/).to_s.capitalize
+            redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
+        else
+            redirect_to :root, notice: 'Signed in successfully!'
+        end
     end
 end
