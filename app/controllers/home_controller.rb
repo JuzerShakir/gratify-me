42656b73d38595aecbaf94d6f1025c4a85858d7b
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-12 14:46:28 +0530

    Update provider name and removed div

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
     @total = User.all.count
-    if user_signed_in?
-      @user = current_user
-    end
   end
 end
--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -2,14 +2,13 @@ class Users::CallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in @user
-        #session[:user_id] = @user.id
         redirect_to :root, notice: 'Signed in successfully!'
     end
 
     def github
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in @user
-        if @user.provider != 'github'
+        if @user.provider != 'Github'
             redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
         else
             redirect_to :root, notice: 'Signed in successfully!'
