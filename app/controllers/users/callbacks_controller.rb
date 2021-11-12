465d2d98cc1503db46d5afbf81530790f1229a10
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-12 13:49:25 +0530

    no bold flash message, flash message will also show for github users

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -10,9 +10,9 @@ class Users::CallbacksController < Devise::OmniauthCallbacksController
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in @user
         if @user.provider != 'github'
-            redirect_to :root, flash: { headsup: "The email address you have logged in with is associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
+            redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
         else
-            redirect_to :root
+            redirect_to :root, notice: 'Signed in successfully!'
         end
     end
 end
