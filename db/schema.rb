70905b2886da4d858295402d32d033e7541804ee
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-13 17:47:36 +0530

    Deleted database Removed notes from schema migration

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2021_11_13_052336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "notes", force: :cascade do |t|
-    t.text "say"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_notes_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,5 +31,4 @@ ActiveRecord::Schema.define(version: 2021_11_13_052336) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "notes", "users"
 end
