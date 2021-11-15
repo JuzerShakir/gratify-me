ae3f275e3c89efb8764c3c8367456a2f66a7f89e
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-15 22:38:44 +0530

    Modified code with same logic

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -1,42 +1,47 @@
 class Users::CallbacksController < Devise::OmniauthCallbacksController
+    # a callback before running any provider function
     before_action :load_user
 
+
+    # respective provider function will run based on what provider user logged in from
     def google_oauth2
-        if @user.provider == __method__.match(/[a-z]+/).to_s.capitalize
-            correct_provider
-        else
-            provider_already_exists
-        end
+        # __method__ will provide us with the name of the provider he logged in from
+        # then we apply a little regexp to match the with the name set in database
+        provider_method = __method__.match(/[a-z]+/).to_s.capitalize
+        # we call function to check if user has logged in from correct provider
+        check_provider(provider_method)
     end
 
     def github
-        if @user.provider == __method__.to_s.capitalize
-            correct_provider
-        else
-            provider_already_exists
-        end
+        provider_method = __method__.to_s.capitalize
+        check_provider(provider_method)
     end
 
     def twitter
-        if @user.provider == __method__.to_s.capitalize
-            correct_provider
-        else
-            provider_already_exists
-        end
+        provider_method = __method__.to_s.capitalize
+        check_provider(provider_method)
     end
+
 
     private
 
-    def load_user
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in @user
-    end
+        # this function will load user from social app or provider and sign them in through devise function sign_in
+        def load_user
+            # 'from_omniauth' defined in user model
+            # request.env loads user details from the provider
+            @user = User.from_omniauth(request.env["omniauth.auth"])
+            sign_in @user
+        end
 
-    def provider_already_exists
-        redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
-    end
-
-    def correct_provider
-        redirect_to :root, notice: 'Signed in successfully!'
-    end
+        def check_provider(provider_method)
+            # user is now logged in
+            # if provider value of a user from database matches the provider name he logged in from ..
+            if @user.provider == provider_method
+                #..then show him a successful login message
+                redirect_to :root, notice: 'Signed in successfully!'
+            else
+                # else show message that user has logged in from wrong provider
+                redirect_to :root, flash: { headsup: "The email address you have logged in with is already associated with #{@user.provider} in our database. Hence, you're logged in via #{@user.provider}"}
+            end
+        end
 end
