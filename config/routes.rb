6ca1f9ffa9b3ea2a5a523a93612c1103c2b25946
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-11-15 20:51:11 +0530

    Align attributes and give mixin and variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
+  # create custom links and route helpers for say controller
   get '/new', to: 'says#new'
   post '/create', to: 'says#create'
   get '/quote/:id', to: 'says#edit', as: :edit_quote
   put '/quote/:id', to: 'says#update'
   delete '/quote/:id', to: 'says#destroy', as: :delete_quote
 
-  devise_scope :user do
-    # since we aren't using devices sign_in and sign_up url we redirect to root path
-    get 'register', 'sign_up', 'sign_in', 'signin', to: 'home#not_found'
-  end
-
+  # loads routes for registeration controller and omniauth
+  # we remove '/users' from the link string with the path attribute
+  # since we have modified registeration and omniauth controllers we explicitly tell routes to run those
   devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: "users/callbacks" }, path: ''
 
+  # homepage
   root to: 'home#index'
 
+  # if user manually tries to enter a url that isnt mentioned in routes, redirect to appropriate action
   get ':not_found', to: 'home#not_found', constraints: { not_found: /.*/ }
 
 end
